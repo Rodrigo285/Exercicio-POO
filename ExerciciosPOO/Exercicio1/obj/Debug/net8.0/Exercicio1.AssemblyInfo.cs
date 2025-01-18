8666732049ed41acb9834fd52a642a7dfdba2039
Rodrigo Borges <borgesenfermagem@gmail.com>
2025-01-18 19:51:25 -0300

    chore(): atualiza arquivo da solucao ExerciciosPOO sln

--- ExerciciosPOO/Exercicio1/obj/Debug/net8.0/Exercicio1.AssemblyInfo.cs
+++ ExerciciosPOO/Exercicio1/obj/Debug/net8.0/Exercicio1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Exercicio1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e71c0c419fc701cfdd1e6d0fc062f30cb85d8bf8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ad696af16f2c5f503cb51cb81247aa6a5b40c68")]
 [assembly: System.Reflection.AssemblyProductAttribute("Exercicio1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Exercicio1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

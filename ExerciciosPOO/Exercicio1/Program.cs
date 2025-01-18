026443fbb165d770c23dda472b6932043c0caea9
Rodrigo Borges <borgesenfermagem@gmail.com>
2025-01-18 17:36:55 -0300

    feat(Exercicio1): adiciona calculo da area de triangulo com programacao procedural

--- ExerciciosPOO/Exercicio1/Program.cs
+++ ExerciciosPOO/Exercicio1/Program.cs
@@ -1,9 +1,43 @@
 ﻿using System;
+using System.Globalization;
+/*
+ Fazer um programa para ler as medidas dos lados de dois triângulos X e Y (suponha medidas
+ válidas).Em seguida,mostrar o valor das áreas dos dois triângulos e dizer qual dos dois triângulos
+ possui a maior área. 
+ */
 
 namespace MyApp {
     internal class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
-        }
+
+            float xa, xb, xc, ya, yb, yc;
+
+            Console.WriteLine("Entre com as medidas do triangulo X ");
+            xa = float.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            xb = float.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            xc = float.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+
+            Console.WriteLine("Entre com as medidas do triangulo Y ");
+            ya = float.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            yb = float.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            yc = float.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            float p = (xa + xb + xc) / 2;
+            double areax = Math.Sqrt(p * (p - xa) * (p - xb) * (p - xc));
+
+            p = (ya + yb + yc) / 2;
+            double areay = Math.Sqrt(p * (p -   ya) * (p - yb) * (p - yc));
+
+            Console.WriteLine($"Area de X {areax.ToString("F4", CultureInfo.InvariantCulture) }");
+            Console.WriteLine($"Area de Y {areay.ToString("F4", CultureInfo.InvariantCulture)}");
+
+            if (areax > areay) {
+                Console.WriteLine("Maoir area X");
+            }
+            else {
+                Console.WriteLine("Maior area Y");
+            }
+        }//fim main
     }
 }

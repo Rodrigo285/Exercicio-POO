1d4c240a5818d89e1f8efa3e5957dc597d7880bd
Rodrigo Borges <borgesenfermagem@gmail.com>
2025-01-19 18:48:31 -0300

    feat(Exercicio3):  adiciona calculo de salario medio de funcionarios

---
+++ ExerciciosPOO/Exercicio3/Funcionario.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+
+
+namespace Exercicio3 {
+    internal class Funcionario {
+
+        public string nome;
+        public double salario = 0;
+
+        public double salarioMedio(double salario1, double salario2) {
+
+            return (salario1 + salario2) / 2;
+        }
+    }
+}
---
+++ ExerciciosPOO/Exercicio3/Program.cs
@@ -0,0 +1,28 @@
+﻿using Exercicio3;
+using System;
+using System.Globalization;
+
+
+namespace Exercicio3 {
+    internal class Program {
+        static void Main(string[] args) {
+            
+            Funcionario func1 = new Funcionario();
+            Funcionario func2 = new Funcionario();  
+            
+            Console.WriteLine("Dados do primeiro funcionario");
+            Console.Write("Nome: ");
+            func1.nome = Console.ReadLine();   
+            Console.Write("Salario: ");
+            func1.salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Dados do segundo funcionario");
+            Console.Write("Nome: ");
+            func2.nome = Console.ReadLine();
+            Console.Write("Salario: ");
+            func2.salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine($"Salario Medio = {func1.salarioMedio(func1.salario, func2.salario).ToString("F2", CultureInfo.InvariantCulture)}");
+        }
+    }
+}
